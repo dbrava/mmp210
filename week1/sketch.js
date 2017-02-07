bf85a7f14f7ec328eab113f922a57234cba5adbd
dbrava <brava.des@gmail.com>
2017-02-07 17:23:11 -0500

    no arc

--- week1/sketch.js
+++ week1/sketch.js
@@ -5,31 +5,29 @@ function setup(){
     background('lightblue');
     
     fill('white');
-    
     ellipse(250, 250, 250);
+    
     fill('black');
     ellipse(210, 230, 55, 55);
     ellipse(290, 230, 55, 55);
+    
     fill('white');
     ellipse(290, 220, 20, 20);
     ellipse(210, 220, 20, 20);
     
+    ellipse(250, 240, 10, 10);
+    
+    fill('red');
+    ellipse(250, 260, 20, 20);
   
     
-    line(20, 90, 60, 75);
-    line(20, 90, 60, 75);
     
-   
-  
     stroke(200, 0, 100);
-    fill('white');
-    arc(150, 200, 60, 75, 0, PI );
-    arc(130, 200, 60, 75, 0, PI );
    
     
    
     
-      fill("#f4d85d");
+    fill("#f4d85d");
     rect(0, 280, 640, 240);
     
   
