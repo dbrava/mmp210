507ea539bc5fcf5483c5db44a50b66a3d90d3a8b
dbrava <desiree.dias@ll03-32-33500.bmcc.cuny.edu>
2017-03-07 13:23:35 -0500

    adds
    more adds

--- week4/sketch.js
+++ week4/sketch.js
@@ -3,34 +3,42 @@
 
 function setup(){
     createCanvas(600, 400);
-   
+    //background
+    background("black");
+
     
-  
-    frameRate(30);
-    textSize(80);
-    textSize(100);
-    textAlign(CENTER);
 }
 
 function draw() {
-  background("black");
+  
+    stroke(255);
+    frameRate(20);
+
+var x= width/2;
+var y= 100;
+  
   text(frameCount, width/2, height/2)
-    
-    
-  if (frameCount < width/3) {
-        fill ("white");
+    if (frameCount < width/2) {
+        fill ("white"); 
     }else{ 
-        fill("red");
+        fill("red"); 
     }   
-   
 
-    
-    textSize(50);
-text("Tick", width/2, 130);
+textSize(120);
+noStroke();
+text("Tick", x, 100);
+
 fill(0, 102, 153);
-text("Tack", 200, 160);
-fill(0, 102, 153, 151);
-text("Tick", 100, 190);
+text("Tick", x + 10 , y * 3);
 
-    
+fill(0, 102, 153, 151);
+text("Tack", x / 8, y * 2);
+
+
+
+
+//ellipse
+
+ellipse(mouseX, mouseY, 100,100,100);
+
 }
