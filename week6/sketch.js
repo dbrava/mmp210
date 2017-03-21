61838b4a2ccad5627aedffda368d6303158fa7bb
dbrava <desiree.dias@f706-04-34139.bmcc.cuny.edu>
2017-03-21 17:41:34 -0400

    animation

--- week6/sketch.js
+++ week6/sketch.js
@@ -1,36 +1,80 @@
 //animation
-var x = 10;
-var y = 10;
+var r = 0;
+var x = 210;
+var x1 = 290;
+var y = 220;
+var a1 = 350;
+var a = 150;
+var rot = 0;
 
-var r = 0, g = 0, b = 0;
-
-function setup() {
-	createCanvas(640, 480);
+function setup(){
+    createCanvas(640, 480);
 }
 
-function draw(){
-//	background(200);
+function draw() {   
+    background('lightblue');
+    
+    fill('white');
+	stroke("black");
+
 	
-  if (frameCount % 60 == 0){	
-	 r = random(0, 255);
-	 g = random(0, 255);
-	 b = random(0, 255);
-  }
+	//face
+    ellipse(250, 250, 250);
+    
+    fill('black');
+	//left eye
+    ellipse(210, 230, 55, 55);
+	//right eye
+    ellipse(290, 230, 55, 55);
+    
 	
-	fill(r, g, b);
+	//nose
+    ellipse(250, 240, 10, 10);
+    
+    fill('red');
+	//mouth
+    ellipse(250, 260, 20, 20);
+  
+    
+    fill('black');
+    stroke("black");
+	//right ear
+    arc(a1, 150, 60, 60, 0, PI, OPEN);
+
+	//left ear
+	push();
+	translate(a,60);
+	rotate(rot);
+	rot += 0.01;
+    arc(a, 150, 60, 60, 0, PI, OPEN);
+	pop();
 	
-	ellipse(x, y, 50);
-	x += 1;
 	
-	if (x > width){
-		x = 0;
-	}
+
 	
-	if (x > width/2) {
-		y--;
-	}else{
-		y++;
-	}
+    fill('white');
+	noStroke();
+	//right pupil
+	push();
+	translate(x1, y);
+	rotate(r);
+	r += 0.01;
+    ellipse(10, 10, 20, 20);
+	pop();
 	
-	
+	//left pupil
+	push();
+	translate(x, y);
+	rotate(r);
+
+    ellipse(10, 10, 20, 20);
+	pop();
+   
+
 }
+
+    
+    
+    
+
+
---
+++ week6/transform.js
@@ -0,0 +1,20 @@
+//transform animation
+
+function setup(){
+	createCanvas(640, 480);
+	noStroke();
+}
+
+function draw(){
+	
+	background(0);
+	
+	translate(width/2, height/2);
+	
+	fill("green");
+	ellipse
+	
+	
+
+}
+    
