35ade8fe40a3c38d5afa055b5bd1533e5d664ae7
dbrava <desiree.dias@f706-04-34139.bmcc.cuny.edu>
2017-04-25 17:35:11 -0400

    fixes window defined function week 7

--- week7/sketch.js
+++ week7/sketch.js
@@ -9,7 +9,6 @@ function draw(){
    background(183,158,200); //purple
 
    
-
    var windowWidth = width/5; // window width
    var windowHeight = height/3; // window height
  
@@ -19,7 +18,7 @@ function draw(){
 	var x1 = 150;
 	var y1 = 150;
 	for (var i = 0; i < 4; i++) {
-    for (var h = 0; h < 2; h++) {
+       	for (var h = 0; h < 2; h++) {
             nova(x + x1 * i, y + y1 * h, windowWidth, windowHeight);
 			nova(x + x1 * i, y * 30 + y1 * h, windowWidth/2, windowHeight/2);
         }
@@ -31,16 +30,7 @@ function draw(){
 //name new function nova
 function nova(x, y, windowWidth, windowHeight) {
 var Trim = color(237,235,216);// window trim
-
-   var windowWidth = width/4; // window width
-   var windowHeight = height/3; // window height
- 
-   var Trim = color(237,235,216);// window trim
-   
-   for ( var x = windowWidth/4; x < width; x+=windowWidth) {
-   for (var y = windowHeight/4; y < height; y+=windowHeight) {
-   
-
+	
    fill(Trim);
    stroke(Trim);    
    rect(x-5, y-5 , windowWidth/2 +10, windowHeight/2+10 );
@@ -72,41 +62,7 @@ var Trim = color(237,235,216);// window trim
    rect(x, y, windowWidth/6, windowHeight/4);
    rect(x, y,windowWidth/3, windowHeight/4);
    rect(x, y, windowWidth/2, windowHeight/8);
-
    rect(x, y, windowWidth/2, windowHeight/4);
        
    
  }
-
-   rect(x, y, windowWidth/2, windowHeight/4);      
- }
-   }
-
-
-function drawWindow(x,y,windowWidth,windowHeight){
-   fill("Trim");
-        rect(x,y,windowWidth/2,windowHeight/2);
-           
-       fill("#5F2847 ");
-       stroke("#F2F1D2 ");  
-       
-       strokeWeight(6);
-   
-           
-       rect(x, y + windowWidth/2,windowWidth/2, windowHeight/4 );
-       rect(x, y + windowWidth/2,windowWidth/2, windowHeight/8 );
-
-       rect(x + windowWidth/6, y + windowWidth/2,windowWidth/6, windowHeight/4 );
-       
-       //up
-       fill("#823864 ");
-       
-           
-       rect(x, y, windowWidth/2, windowHeight/4);
-       rect(x, y,  windowWidth/2, windowHeight/8);
-       rect(x, y+windowWidth/3,  windowWidth/2, windowHeight/8);
- 
-     
-   
-}
-
